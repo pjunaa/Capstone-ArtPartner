2f3d0ea43224c9eb0001fbc3f885fddee8b3ee53
pjunaa <149715293+pjunaa@users.noreply.github.com>
2024-11-08 16:16:29 +0900

    Update api_constants.dart

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,15 +1,15 @@
-String BASE_URL = "https://test3-east.openai.azure.com";
-String API_KEY = "99afd587586043bdb40109c1c30ced17";
+String BASE_URL = '';
+String API_KEY = '';
 
-String API_KEY_2 = "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";
+String API_KEY_2 = '';
 
-String SEMA_URL = 'http://openapi.seoul.go.kr:8088';
-String SEMA_KEY = '4b464e795670666736365573674462';
-String CLEVELAND_URL = 'https://openaccess-api.clevelandart.org';
-String GOOGLETRNAS_URL = 'https://translation.googleapis.com/language/translate/v2';
-String GOOGLETRANS_KEY="AIzaSyBnqAfG0SQjHSzXiM6X0YErnQF8S0W932I";
+String SEMA_URL = '';
+String SEMA_KEY = '';
+String CLEVELAND_URL = '';
+String GOOGLETRNAS_URL = '';
+String GOOGLETRANS_KEY= '';
 
-String KCISA_URL = 'http://api.kcisa.kr/openapi/API_CCA_145/request';
-String KCISA_KEY = 'acdb3cc4-2fa0-448c-a7a2-0600d01b99aa';
+String KCISA_URL = '';
+String KCISA_KEY = '';
 
-String Deepl_KEY = 'd03bf849-d492-4a3c-933c-d09ee1d24040:fx';
+String Deepl_KEY = '';
